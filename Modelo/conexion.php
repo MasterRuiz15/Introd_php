938719cce30a21438caee6698c3cb02733f89d97
MasterRuiz15 <andresruiz@colegioguanenta.edu.co>
2025-04-29 09:53:28 -0500

    Inicio de sesion

--- Modelo/conexion.php
+++ Modelo/conexion.php
@@ -1,4 +1,34 @@
 <?php
+    // script para crear una conexion con la BD
 
+    // Parametros requeridos para la conexcion con la BD
 
+    // Parametros BD local
+    DEFINE('USER', 'root'); // Crea la constante USER con valor `root`
+    DEFINE('PW', ''); 
+    DEFINE('HOST', 'localhost');
+    DEFINE('BD', 'Empresa');
+
+    // Parametros BD remota (infinityfree)
+    /*DEFINE('USER', 'if0_38542101'); // Crea la constante USER con valor `if0_38542101`
+    DEFINE('PW', 'andres13072008'); 
+    DEFINE('HOST', 'sql306.infinityfree.com');
+    DEFINE('BD', 'if0_38542101_empresa');*/
+
+    // Conexion con la BD
+    $conexion = mysqli_connect(HOST, USER, PW, BD);
+
+    // Establecer conjunto de caracteres para el hosting
+    mysqli_set_charset($conexion, "utf8mb4");
+
+    // Verificar la conexion con la BD
+    if(!$conexion)
+    {
+        die("La conexion con la BD fallo: " + mysqli_error($conexion));
+        exit();
+    }
+    /*else
+    {
+        die("Conexion a la BD exitosa!");
+    }*/
 ?>
